823dac1b6ad9cd912fe60a6f792b194b4633a62e
Miguel Diaz <mwd1@posteo.net>
2023-09-12 01:35:42 -0700

    Changed variables to readonly and added umount

--- bootdev-poweroff.sh
+++ bootdev-poweroff.sh
@@ -3,11 +3,12 @@
 # Description: Poweroff/unmount a boot partition that is on a removable device such as a USB
 
 boot_dev_uuid="$(awk '/\/boot/ {print $1}' /etc/fstab | sed 's/UUID=//')"
-boot_dev="/dev/disk/by-uuid/$boot_dev_uuid"
+readonly boot_dev_uuid
+readonly boot_dev="/dev/disk/by-uuid/$boot_dev_uuid"
 
 if ! awk '/\/boot/ { print $4 }' /etc/fstab | grep noauto >/dev/null; then
-  echo "Update /etc/fstab to auto unmount the boot device"
-  exit
-else
-  udisksctl power-off -b "$boot_dev"
+  echo "Update /etc/fstab to auto unmount the boot device by adding the noauto option"
+  umount "$boot_dev"
 fi
+
+udisksctl power-off -b "$boot_dev"
